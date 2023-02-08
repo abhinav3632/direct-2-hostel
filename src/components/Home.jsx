930da9978558824330cee739df40e0dc595e814a
Abhinav Arun <abhinavarun3632@gmail.com>
2023-02-08 19:00:31 +0530

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,9 +7,9 @@ export const Home = () => {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b from-orange-400 via-orange-300 to-indigo-400"
+      className="h-screen w-full bg-gradient-to-b from-orange-300  to-orange-200"
     >
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full md:flex-row">
+      <div className="gap-x-4 max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full md:flex-row">
         <div className="flex flex-col justify-center ">
           <h2 className="text-4xl sm:text-7xl font-bold text-slate-900">
             Direct 2 Hostel
@@ -25,7 +25,7 @@ export const Home = () => {
               to="portfolio"
               smooth
               duration={500}
-              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gray-600 cursor-pointer"
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-indigo-500 cursor-pointer"
             >
               Get Started
               <span className="group-hover:rotate-90 duration-300">
@@ -38,7 +38,7 @@ export const Home = () => {
         <div>
           <img src={HeroImage}
             alt="my profile"
-            className="rounded-2xl my-5 shadow-lg mx-auto w-2/3 md:w-full md:mx-8"
+            className="rounded-2xl my-5 shadow-lg mx-auto w-2/3 md:w-full"
           />
         </div>
       </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,8 +3,23 @@ import React, { useState } from 'react'
 import d2hlogo from "../asset/Untitled-1.png";
 // import { FaBars, FaTimes } from "react-icons/fa";
 // import { Link } from "react-scroll";
+import './Nav.css';
 
 export const Nav = () => {
+    //change nav color when scrolling
+    const [color, setColor] = useState(false)
+    const changeColor = () =>{
+        if(window.scrollY >= 90){
+            setColor(true);
+        }
+        else{
+            setColor(false)
+        }
+    }
+
+    window.addEventListener('scroll', changeColor)
+
+
     let Links = [
         { name: "Home", link: "/" },
         { name: "About", link: "/" },
@@ -12,36 +27,36 @@ export const Nav = () => {
         { name: "Pricing", link: "/" },
         { name: "Contact", link: "/" }
     ];
-    let [open,setOpen]=useState(false);
+    let [open, setOpen] = useState(false);
 
     return (
         <div>
-            <div className='w-full fixed top-0 left-0 bg-orange-400'>
-                <div className='md:flex items-center justify-between py-0.5 bg-transparent md:px-10 px-7'>
-                    <div className='font-bold font-[Poppins] text-xl  text-slate-800 cursor-pointer flex items-center'>
-                        <span className='mr-1'>
-                            <img src={d2hlogo} className='w-full h-20' alt="" />
-                        </span>
-                        {/* Direct 2 Hostel */}
-                    </div>
-                    <div onClick={()=> setOpen(!open)} className='text-xl absolute right-8 top-6 cursor-pointer md:hidden'>
-                        <i class={open ? "fa-solid fa-xl fa-x" : "fa-solid fa-xl fa-bars"}></i>
-                    </div>
-                    <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] px-6 right-0 w-half bg-orange-400 rounded-md  md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in-out ${open ? 'top-20': 'top-[-500px]'}`}>
-                        {
-                            Links.map((link) => (
-                                <li key={link.name} className='md:ml-8 text-2xl md:my-0 my-7'>
-                                    <a href={link.link} className='text-gray-100 hover:text-gray-800 duration-500'>{link.name}</a>
-                                </li>
-                            ))
-                        }
-                        {/* <Button>
+            <div className={color ? 'w-full fixed top-0 left-0 bg-white' : 'w-full fixed top-0 left-0 bg-tranparent'}>
+                    <div className='md:flex items-center justify-between py-0.5 bg-transparent md:px-10 px-7'>
+                        <div className='font-bold font-[Poppins] text-xl  text-slate-800 cursor-pointer flex items-center'>
+                            <span className='mr-1'>
+                                <img src={d2hlogo} className='w-full h-20' alt="" />
+                            </span>
+                            {/* Direct 2 Hostel */}
+                        </div>
+                        <div onClick={() => setOpen(!open)} className='text-xl absolute right-8 top-6 cursor-pointer md:hidden'>
+                            <i class={open ? "fa-solid fa-xl fa-x" : "fa-solid fa-xl fa-bars"}></i>
+                        </div>
+                        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] px-6 right-0 w-half bg-transparent rounded-md  md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in-out ${open ? 'top-20' : 'top-[-500px]'}`}>
+                            {
+                                Links.map((link) => (
+                                    <li key={link.name} className='md:ml-8 text-2xl md:my-0 my-7'>
+                                        <a href={link.link} className='text-gray-900 hover:text-gray-500 duration-500'>{link.name}</a>
+                                    </li>
+                                ))
+                            }
+                            {/* <Button>
                             Sign Up For Free
                         </Button> */}
-                    </ul>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
     )
 }
 
